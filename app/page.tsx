66df5bf2aa976eef425a0e947a6963d5663fdac9
Mahmoud <mahmoud-bensaleh@outlook.com>
2024-01-03 17:24:31 +0200

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
-import { lusitania } from './ui/fonts';
+import { lusitana } from './ui/fonts';
 import Image from 'next/image';
 export default function Page() {
   return (
@@ -12,7 +12,7 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div
-          className={`${lusitania.className} flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20`}
+          className={`${lusitana.className} flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20`}
         >
           <div className={styles.shape} />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
